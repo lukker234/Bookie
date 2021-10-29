0bffe4d10823334de370729a35c891086d653b79
Luc Daalmeijer <lucd877@gmail.com>
2021-10-29 14:45:00 +0200

    Initial Commit

---
+++ Shared/BookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookieApp.swift
+//  Shared
+//
+//  Created by Luc Daalmeijer on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
